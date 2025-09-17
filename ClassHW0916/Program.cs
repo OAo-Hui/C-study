8ea8cb2bc0c6a90f445d4b317fac398b0740267e
user <user@201-13>
2025-09-17 10:24:09 +0800

    加入專案檔案。

---
+++ ClassHW0916/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassHW0916
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name =　"11144225 蔡曉慧" ;
+            Console.WriteLine(name);
+            Console.ReadLine();
+        }
+    }
+}
